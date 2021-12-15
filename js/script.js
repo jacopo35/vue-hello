916b10b5279a9b0bbcb8289790d9f1bb5fe84234
jacopo35 <jaczor00@gmail.com>
2021-12-15 15:24:54 +0100

    add-jsvue-command

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@
 Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 */
+const app = new Vue(
+ {
+   el: '#datas', 
+   data: { 
+   message: 'Questo è un messaggio di test in un h1',
+   image: 'https://www.aiapp.net/wp-content/uploads/2020/09/Agricoltura.jpg'
+    }
+ }
+);
