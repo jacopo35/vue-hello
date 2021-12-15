45d02e30194d9382558519c4cb4c9e3ba90aaece
jacopo35 <jaczor00@gmail.com>
2021-12-15 15:26:46 +0100

    div-change name in html

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 */
 const app = new Vue(
  {
-   el: '#datas', 
+   el: '#root', 
    data: { 
    message: 'Questo è un messaggio di test in un h1',
    image: 'https://www.aiapp.net/wp-content/uploads/2020/09/Agricoltura.jpg'
